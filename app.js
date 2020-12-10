8d50e18da96f2f2e4b5fb5a4161ed2d673002444
gucci8 <kusti.hamalainen@aalto.fi>
2020-12-10 20:29:40 +0200

    VITTU TOIMI

--- app.js
+++ app.js
@@ -23,4 +23,10 @@ app.use(middleware.authMiddleware);
 
 app.use(router.routes());
 
-app.listen({ port: 7777 });
+let port = 7777;
+if (Deno.args.length > 0) {
+    const lastArgument = Deno.args[Deno.args.length - 1];
+    port = Number(lastArgument);
+}
+
+app.listen({ port: port });
