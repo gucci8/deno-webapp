32bba0bea749480ea7327b985ae222f789e65533
gucci8 <60006237+gucci8@users.noreply.github.com>
2021-01-21 18:34:30 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,11 +6,7 @@ if (Deno.env.get('TEST_ENVIRONMENT')) {
     config.database = {};
   } else {
     config.database = {
-      hostname: "hattie.db.elephantsql.com",
-      database: "sevnishb",
-      user: "sevnishb",
-      password: "zAU7BtCem2FUzO-3aLzGxzRVDsV7iMpr",
-      port: 5432
+      //tässä olisi testausta varten tietokannan salasanat jne. mutta git ei anna pitää niitä näkyvillä
     }
   }
 
